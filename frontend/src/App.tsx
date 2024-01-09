07fefb5c6ec0a85d67b83c034e34afc4a5765b26
SherwynNg <sherwyn2000@hotmail.com>
2024-01-09 15:22:44 +0800

    List-Products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { sampleProducts } from './data';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
       <header>TS Amazona</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              ></img>
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All rights reserved</footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
